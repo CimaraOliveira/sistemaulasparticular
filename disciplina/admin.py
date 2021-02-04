86b1e063c2a34e4668bc02a50363bb1df7671a75
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-02-04 14:57:49 -0300

    pagina home usuario

--- disciplina/admin.py
+++ disciplina/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'titulo', 'professor', 'descricao_longa']
+
+
+
+admin.site.register(models.Disciplina, DisciplinaAdmin)
+admin.site.register(models.Professor)
+admin.site.register(models.Usuario)
+
+
+
