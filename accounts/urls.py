55d75b252391bb0b081fe84233b0c897ccb9713f
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-03-13 11:17:05 -0300

    testando enviar deploy

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,17 @@ from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'registration'
+
 urlpatterns = [
 
    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="accounts/password-reset.html"), name='password-reset'),
-   path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/reset_password_sent.html"),  name='reset_password_done'),
+   path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
+        name='password-reset-done'),
    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_comfirm'),
-   #path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_confirm'),
-   path('reset_password-complete/', auth_views.PasswordChangeView.as_view(template_name="accounts/reset_password-complete.html"), name='password_reset_done'),
+   path('reset_password-complete/', auth_views.PasswordChangeView.as_view(template_name="accounts/reset_password-complete.html"), name='password_reset_complete'),
+
 
 
 ]
