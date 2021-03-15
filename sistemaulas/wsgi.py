801b95b555b78c3d3658a99308ffa1e68bf1a8f0
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-03-15 19:14:35 -0300

    testes deploy

--- sistemaulas/wsgi.py
+++ sistemaulas/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistemaulas.settings.base')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistemaulas.settings.heroku')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistemaulas.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistemaulas.settings.heroku')
 
 application = get_wsgi_application()
